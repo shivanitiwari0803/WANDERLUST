8c588b3f81eba18ef8d7f3d3270ea9b3b7df60a3
shivani tiwari <shivanitiwari0803@gmail.com>
2025-06-16 21:57:40 +0530

    updated code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listings.js");
+const path = require("path")
+const methodOverride = require("method-override")
 
 
 
@@ -19,24 +21,77 @@ async function main() {
     
 }
 
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended : true}))
+app.use(methodOverride("_method"))
 
 
 app.get("/",(req,res)=>{
     res.send("hello i am back again after my exams")
 })
 
-app.get("/testlistings", async (req,res)=>{
-    let sampleListing = new Listing({
-        title : "My New House",
-        Description : "by the road",
-        price : 3400,
-        location :"paris",
-        country : "Europe"
-    })
-   await sampleListing.save();
-   console.log("sample was saved ")
-   res.send("successfully saved")
+//index route
+app.get("/listings",async(req,res)=>{
+    const allListings = await Listing.find({})
+    res.render("listings/index", { allListings })  
+    
 })
+
+
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new")
+})
+
+//show route
+app.get("/listings/:id", async (req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/show", { listing })  
+})
+
+//create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+     await newListing.save();
+     res.redirect("/listings")
+
+})
+//edit route
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/edit", { listing }) 
+})
+
+// update route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedlisting = await Listing.findByIdAndDelete(id)
+    res.redirect("/listings")
+})
+
+
+// app.get("/testlistings", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My New House",
+//         Description : "by the road",
+//         price : 3400,
+//         location :"paris",
+//         country : "Europe"
+//     })
+//    await sampleListing.save();
+//    console.log("sample was saved ")
+//    res.send("successfully saved")
+// })
 
 
 
--- models/listings.js
+++ models/listings.js
@@ -4,13 +4,13 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title: {
         type: String,
-        required: true
     },
-    Description: String,
+    description: String,
     image: {
-        type: String,
-        default : "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        filename: String,
+        url : String,
+        // default : "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        // set: (v) => v === "" ? "https://images.unsplash.com/photo-1628744448840-55bdb2497bd4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
     price: Number,
     location: String,
